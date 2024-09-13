738017ae9db589c653f742fa2774b99eee3fb9cb
fatihdeniz <fatihdeniz@gmail.com>
2024-09-13 13:37:34 +0300

    Add files via upload

---
+++ src/loader_hetero.py
@@ -0,0 +1,104 @@
+import os
+import torch
+import numpy
+from torch_geometric.data import Data, HeteroData
+from torch_geometric.loader import NeighborLoader
+from torch_geometric.transforms import ToUndirected
+
+from src.dataset import DataProcess
+from src.utils import gen_xavier
+from src.loader import Loader
+
+class HeteroLoader(Loader): 
+    def __init__(self, nodes_file, edges_file, feature_names, args):
+        args.hetero = True
+        super(HeteroLoader, self).__init__(nodes_file, edges_file, feature_names['domain'], args)
+        self.data = self.__prepare_hetero_data(feature_names, args)
+        # print(self.data)
+        # print(self.val_index_tensor)
+        # print(self.test_index_tensor)
+        self.data['domain'].val_index_tensor = self.val_index_tensor
+        self.data['domain'].test_index_tensor = self.test_index_tensor
+
+    def __prepare_hetero_data(self,feature_names, args):
+        domain_features = torch.tensor(self.nodes_df.loc[self.nodes_df['feat_domain'] == 1, feature_names['domain']].values.tolist(), dtype=torch.float)
+        if feature_names.get('ip', False):
+            ip_features = torch.tensor(self.nodes_df.loc[self.nodes_df['feat_ip'] == 1, feature_names['ip']].values.tolist(), dtype=torch.float)
+        else:
+            ip_features = torch.ones(self.nodes_df[self.nodes_df['feat_ip'] == 1].shape[0],1)
+
+        subnet_features = torch.ones(self.nodes_df[self.nodes_df['feat_subnet'] == 1].shape[0],1)
+        # asn_features = torch.tensor(self.nodes_df.loc[self.nodes_df['feat_asn'] == 1, feature_names].values.tolist(), dtype=torch.float)
+        # city_features = torch.tensor(self.nodes_df.loc[self.nodes_df['feat_city'] == 1, feature_names].values.tolist(), dtype=torch.float)
+
+        edge_index = []
+        for i in range (5):
+            edges = torch.tensor(self.edges_ind.loc[self.edges_ind['edge_type'] == i, ['source', 'target']].values.tolist(), dtype=torch.long).t().contiguous()
+            # print(edges.size())
+            if edges.size()[0] == 0:
+                edges = torch.empty(2, 0, dtype=torch.long)
+            edge_index.append(edges)
+        
+        self.y = torch.tensor(list(self.nodes_df.loc[self.nodes_df['feat_domain'] == 1, 'label']), dtype = torch.long)
+        
+        if args.use_syn:
+            syn_file = args.syn_file
+            syn_labels = args.syn_labels
+            
+            size0, size1, size2 = domain_features.size(0), ip_features.size(0), subnet_features.size(0)
+            if not os.path.exists(syn_file):
+                print(domain_features.size())
+                node_count = size0 + size1 + size2
+                gen_xavier(xavier_file = syn_file,
+                         node_count=node_count, feature_count=len(syn_labels))
+            syn_data = DataProcess.load_nodes_from_path(syn_file)[syn_labels].values.tolist()
+            domain_features = torch.cat((domain_features, torch.tensor(syn_data[:size0], dtype=torch.float)), 1)
+            ip_features = torch.cat((ip_features, torch.tensor(syn_data[size0:size0+size1], dtype=torch.float)), 1)
+            subnet_features = torch.cat((subnet_features, torch.tensor(syn_data[size0+size1:], dtype=torch.float)), 1)
+        
+        data = HeteroData()
+        
+        data['domain'].x = domain_features  # domain feature matrix
+        data['ipp'].x = ip_features          # edge feature matrix
+        data['subnet'].x = subnet_features  # subnet feature matrix
+        # data['asn'].x = asn_features  # asn feature matrix
+        # data['city'].x = city_features  # city feature matrix
+
+        # for edge_type in edge_index:
+        #     print(edge_type.size())
+        # domains and ips are connected by an edge
+        data['domain', 'to', 'ipp'].edge_index = edge_index[0]   # shape: [2, num_edges]
+        data['ipp', 'to', 'subnet'].edge_index = edge_index[1]   # shape: [2, num_edges]
+        
+        data['domain', 'fqdnapex', 'domain'].edge_index = edge_index[2]   # shape: [2, num_edges]
+        # reverse_edges = edge_index[2].clone().flip(0)
+        # data['domain', 'fqdnapex', 'domain'].edge_index = torch.cat([edge_index[2], reverse_edges], dim=1)
+        
+        # temp_apexfqdn = edge_index[2].clone()
+        # temp_source = temp_apexfqdn[0].clone()
+        # temp_apexfqdn[0] = temp_apexfqdn[1]
+        # temp_apexfqdn[1] = temp_source
+        # data['domain', 'apexfqdn', 'domain'].edge_index = temp_apexfqdn   # shape: [2, num_edges]
+
+        data['domain', 'similar_apex', 'domain'].edge_index = edge_index[3]   # shape: [2, num_edges]
+        data['domain', 'similar_all', 'domain'].edge_index = edge_index[4]   # shape: [2, num_edges]
+        
+
+        # print(data)
+        # data['subnet', 'to', 'asn'].edge_index = edge_index[3]   # shape: [2, num_edges]
+        # data['ipp', 'to', 'city'].edge_index = edge_index[2]   # shape: [2, num_edges]
+        
+        # adding reverse edges
+        if data.is_directed():
+            data = ToUndirected()(data)
+            print("Data converted to undirected:", not data.is_directed())
+        
+        data['domain'].y = self.y
+        data['domain'].train_mask = self.domain_mask
+        data['domain'].test_mask = self.test_mask
+        data['domain'].validation_mask = self.train_mask
+
+        return data
+
+    
+    
---
+++ src/sage_hetero.py
@@ -0,0 +1,101 @@
+import sys
+from src.learning_curve import LearningCurve 
+import torch
+import torch.nn.functional as F
+from torch_geometric.loader import NeighborLoader
+from tqdm import trange
+
+from src.score import score
+from gnn.heterosage import HeteroGNN, HAN
+from src.experiment import Experiment
+
+
+class Sage_Hetero(Experiment): 
+    def __init__(self, data, args):
+        super(Sage_Hetero, self).__init__(data, args)
+        self.val_index_tensor = data['domain'].validation_mask.nonzero().t().contiguous()[0]
+    
+    def define_batch(self, mask):
+        if self.args.fanout is not None:
+            return NeighborLoader(self.data,
+                num_neighbors=self.args.fanout,
+                # weight_attr= 'edge_weight' if self.args.weighted else None,
+                batch_size=self.batch_size,
+                input_nodes=('domain', mask), # It should be training and testing nodes
+                subgraph_type='induced')
+                                 
+    def get_model(self,data):
+        return self.__prepare_model(data)
+    def __prepare_model(self,data) :
+        
+        if self.args.model_type == 'han':
+            model = HAN(data.metadata(), hidden_channels=self.dim, out_channels=2, 
+                      num_layers=self.num_layers)
+        else:
+            model = HeteroGNN(data.metadata(), hidden_channels=self.dim, 
+                    out_channels=2, num_layers=self.num_layers)
+            
+        return model.to(self.device)
+
+    def train(self):
+        outer_batches = self.define_batch(self.data['domain'].validation_mask)
+        model = self.__prepare_model(self.data)
+        data = self.data.to(self.device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=self.args.lr, weight_decay=self.args.weight_decay)
+        
+        epoch_counter = trange(self.epoch)
+        for epoch in epoch_counter:
+            model.train()
+            optimizer.zero_grad()
+            current_loss = 0
+            for batch in outer_batches:
+                batch = batch.to(self.device)
+                out = model(batch.x_dict, batch.edge_index_dict)
+                loss = F.cross_entropy(out[batch['domain'].validation_mask], batch['domain'].y[batch['domain'].validation_mask])
+                current_loss += loss.item()
+                loss.backward()
+            optimizer.step()
+            
+                
+            if epoch % 10 == 0:
+                val_acc, test_acc = self.__test(model, data, istest=False), self.__test(model, data, istest=True)
+                epoch_counter.set_description(f'''Epoch: {epoch:03d}, Loss: {current_loss:.4f}, Train: {val_acc['acc']:.4f}, Val: {test_acc['acc']:.4f}''')
+        return model
+    
+    @torch.no_grad()
+    def __test(self, model, data_test, istest=True):
+        model.eval()
+
+        test_batches = self.define_batch(data_test['domain'].test_mask if istest else data_test['domain'].validation_mask)
+        all_preds = []
+        all_labels = []
+
+        for batch in test_batches:
+            pred = model(batch.x_dict, batch.edge_index_dict).argmax(dim=1)
+            batch_mask = batch['domain'].test_mask if istest else batch['domain'].validation_mask
+
+            all_preds.extend(pred[batch_mask].tolist())
+            all_labels.extend(batch['domain'].y[batch_mask].tolist())
+
+        return score(all_preds, all_labels)
+
+    
+    @torch.no_grad()
+    def test(self, model_file, data_test, raw_directory='./data/raw_results/', save_embedding=False):
+        modelLoaded = self.__prepare_model(data_test)
+        modelLoaded.load_state_dict(torch.load(model_file))
+        modelLoaded.eval()
+        test_indices = data_test['domain'].test_index_tensor
+
+        if save_embedding:
+            with torch.no_grad():
+                LearningCurve.save_prob_result(f'{self.experiment_id}_{self.extra}', 
+                                            torch.exp(modelLoaded(data_test.x_dict, data_test.edge_index_dict).cpu()), 
+                                            data_test['domain'].y.cpu(), 
+                                            test_indices.cpu(), 
+                                            [], # domain names are passed, I do not jnow why 
+                                            raw_directory)
+
+        return self.__test(modelLoaded, data_test, istest=True)
+#         return self.__test2(modelLoaded, data_test)
+
